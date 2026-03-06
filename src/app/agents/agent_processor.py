af92c3d22dba1f9b5bc5f9d3dd91c5c7293099fc
Alexandra Marie Deany <alexdeany@gmail.com>
2026-03-06 11:46:34 -0800

    Clarify Azure Monitor tracing comment
    Updated comment to clarify Azure Monitor tracing.

--- src/app/agents/agent_processor.py
+++ src/app/agents/agent_processor.py
@@ -20,7 +20,7 @@ from concurrent.futures import ThreadPoolExecutor
 import time
 # from opentelemetry.instrumentation.openai_v2 import OpenAIInstrumentor
 
-# # Enable Azure Monitor tracing
+# # Enable Azure Monitor tracing!
 application_insights_connection_string = os.environ["APPLICATIONINSIGHTS_CONNECTION_STRING"]
 # configure_azure_monitor(connection_string=application_insights_connection_string)
 # OpenAIInstrumentor().instrument()
